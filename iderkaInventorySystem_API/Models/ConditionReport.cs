d686206509f69ec066f11880ab1157cb41a1c7c4
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2025-06-22 02:18:51 -0500

    ConditionReport body

---
+++ iderkaInventorySystem_API/Models/ConditionReport.cs
@@ -0,0 +1,10 @@
+﻿namespace iderkaInventorySystem_API.Models
+{
+    public class ConditionReport
+    {
+        public string TransferId { get; set; }
+        public string User { get; set; }
+        public string Condition { get; set; }
+        public string Details { get; set; }
+    }
+}
